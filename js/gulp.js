eedd0e3e3808bf3ec1b344051121e3fe7497b133
Bernhard <BernhardCermak@gmx.at>
2015-06-15 17:38:20 +0200

    Commit message

---
+++ js/gulp.js
@@ -0,0 +1,145 @@
+// include gulp
+
+var gulp = require('gulp');
+
+
+
+// include plug-ins
+
+var jshint = require('gulp-jshint');
+
+var changed = require('gulp-changed');
+
+var imagemin = require('gulp-imagemin');
+
+//var imageResize = require('gulp-image-resize');
+
+var minifyHTML = require('gulp-minify-html');
+
+var concat = require('gulp-concat');
+
+var stripDebug = require('gulp-strip-debug');
+
+var uglify = require('gulp-uglify');
+
+var autoprefix = require('gulp-autoprefixer');
+
+var minifyCSS = require('gulp-minify-css');
+
+
+
+
+
+// JS hint task
+
+gulp.task('jshint', function() {
+
+    gulp.src('./src/scripts/*.js')
+
+        .pipe(jshint())
+
+        .pipe(jshint.reporter('default'));
+
+});
+
+
+
+// minify new images
+
+gulp.task('imagemin', function() {
+
+    var imgSrc = './src/images/**/*',
+
+        imgDst = './build/images';
+
+
+
+    gulp.src(imgSrc)
+
+        .pipe(changed(imgDst))
+
+//    .pipe(imageResize({
+
+//      width : 300,
+
+//      height : 400,
+
+//      crop : true,
+
+//      upscale : false
+
+//    }))
+
+        .pipe(imagemin())
+
+        .pipe(gulp.dest(imgDst));
+
+});
+
+
+
+// minify new or changed HTML pages
+
+gulp.task('htmlpage', function() {
+
+    var htmlSrc = './src/*.html',
+
+        htmlDst = './build';
+
+
+
+    gulp.src(htmlSrc)
+
+        .pipe(changed(htmlDst))
+
+        .pipe(minifyHTML())
+
+        .pipe(gulp.dest(htmlDst));
+
+});
+
+
+
+// JS concat, strip debugging and minify
+
+gulp.task('scripts', function() {
+
+    gulp.src(['./src/scripts/lib.js','./src/scripts/*.js'])
+
+        .pipe(concat('script.js'))
+
+        .pipe(stripDebug())
+
+        .pipe(uglify())
+
+        .pipe(gulp.dest('./build/scripts/'));
+
+});
+
+
+
+// CSS concat, auto-prefix and minify
+
+gulp.task('styles', function() {
+
+    gulp.src(['./src/styles/*.css'])
+
+        .pipe(concat('styles.css'))
+
+        .pipe(autoprefix('last 2 versions'))
+
+        .pipe(minifyCSS())
+
+        .pipe(gulp.dest('./build/styles/'));
+
+});
+
+
+
+// default gulp task
+
+gulp.task('default', ['imagemin', 'htmlpage', 'scripts', 'styles'], function() {
+
+});
+
+ 
